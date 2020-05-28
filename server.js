1c78c268da57f0e46659563365ad04f67c187554
tscheneider <64435945+tscheneider@users.noreply.github.com>
2020-05-28 13:28:12 -0300

    Configuração do servidor Express com rotas iniciais

---
+++ server.js
@@ -0,0 +1,17 @@
+/* Parte do backend */
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Bem vindo a API ContactKeeper..." })
+);
+
+//Define as rotas
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
