640e04a1053db2e0034e675790abbba2d67b3e24
Thais Scheneieder <tscheneider09@gmail.com>
2020-07-05 11:58:33 -0300

    Primeiro commit
    Primeiro commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,97 +1,18 @@
-const express = require("express"); // framework do servidor
-const router = express.Router(); // cria manipuladores de rota modulares e montáveis
-const bcrypt = require("bcryptjs"); //responsavel pela criptografia
-const jwt = require("jsonwebtoken"); //responsavel por criar novos tokens
-const { check, validationResult } = require("express-validator"); // validador do express
-const config = require("config"); // permite definir um conjunto de parametro e utliza-lo na aplcação
-/* chama o midleware que é o responsável pela descodificação do token id do usuário  */
-const auth = require("../middleware/auth");
-//const { check, validationResult } = require("express-validator/check");
-
-const User = require("../models/User");
+const express = require("express");
+const router = express.Router();
 
 //@route      GET api/auth
 //@desc       Get logged in user
 //@access     Private
-/* função que obtem o usuario logado */
-// Parametro '/' - caminho
-// Parametro auth - verifica a autenticação do token que está em middleware > auth.js
-// Parametro (req, res) - Faz a requisição do usuário no bd
-router.get("/", auth, async (req, res) => {
-  //res.send("Obtem o usuário logado");
-  try {
-    /* Se enviarmos um token certo e estivermos logados, 
-    receberemos um objeto de usuário */
-    //busca os dados através req.user.id e adicionamod todos os dados menos a senha na vaviavel user
-    const user = await User.findById(req.user.id).select("-password");
-    //retorna como resposta o objeto user
-    res.json(user);
-  } catch (err) {
-    //tratamento de erro caso não seja executado os itens a cima
-    console.error(err.message);
-    res.status(500).send("Erro no servidor");
-  }
+router.get("/", (req, res) => {
+  res.send("Obtem o usuário logado");
 });
 
 //@route      POST api/auth
 //@desc       Auth user & get token
 //@access     Public
-// Loga o usuário no sistema (sutentica e obtem o token para acessar o sistema)
-// Parametro '/' - caminho
-// Parametro [check, check] - define a checagem da senha e email
-// Parametro (req, res) - Faz a requisição do usuário no bd
-router.post(
-  "/",
-  [
-    check("email", "Por favor, inclua um email valido").isEmail(), // o email precisa estar no formato de email aaa@aaa.aa
-    check("password", "Senha incorreta").exists(), //verifica se a senha corresponde com a do bd
-  ],
-  async (req, res) => {
-    const errors = validationResult(
-      req
-    ); /* faz a requisição da validação dos resultados (express-validator) */
-    if (!errors.isEmpty()) {
-      // se o erro for diferente de vazio a resposta será o erro 404( 404 - o servidor não pode encontrar o que foi pedido)
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const { email, password } = req.body; //podemos ver o body no postman
-    try {
-      let user = await User.findOne({ email }); //Busca o  email do usuario e adiciona na variavel user
-      if (!user) {
-        //verifica se esse usuario existe e se não existir retorna um usuário existente
-        return res.status(400).json({ msg: "Credencial invalida" });
-      }
-      /* a variavel isMatch recebe se as senha digitada corresponde com a criptografada  */
-      const isMatch = await bcrypt.compare(password, user.password); //realiza a comparação da senha do bd com a senha que está sendo passada
-      if (!isMatch) {
-        //Se as senhas não forem iguais
-        return res.status(400).json({ msg: "Credencial invalida" }); // retorna erro 404( 404 - o servidor não pode encontrar o que foi pedido)
-      }
-      //cria um objeto payload com um objeto de usuario
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      /* Cria um token valido através de uma palavra secreta */
-      jwt.sign(
-        payload, //objeto que possui um usuário
-        config.get("jwtSecret"), //pega a palavra secreta que pasta config no arquivo default.json
-        {
-          /* A configuração insíra em 1 hora */
-          expiresIn: 360000,
-        }, //cria função passando err e token
-        (err, token) => {
-          if (err) throw err; //se obtiver erro retorna o erro
-          res.json({ token }); //tras como resposta o token
-        }
-      );
-    } catch (err) {
-      // caso o try não funcione
-      console.error(err.message);
-      res.status(500).send("Erro no servidor");
-    }
-  }
-);
+router.post("/", (req, res) => {
+  res.send("Logar usuário");
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,103 +1,11 @@
-/* Requeremos o express para que seja possivel utilizar o roteamento */
 const express = require("express");
-/* cria-se uma variavel chamada router e nelas será adicionado o roteador do express */
 const router = express.Router();
-/* importa a biblioteca de criptografia */
-const bcrypt = require("bcryptjs"); //responsavel pela criptografia
-//responsavel por criar novos tokens
-const jwt = require("jsonwebtoken");
-/* realiza a validaçõe e verifica campos */
-const { check, validationResult } = require("express-validator");
-//o config permite que acessamos o default.json que possui a palavra secreta do token
-const config = require("config");
-//const { check, validationResult } = require("express-validator/check");
-
-//importa como deve ser o modelo do usuário que está definido models/User
-const User = require("../models/User");
 
 //@route      POST api/users
 //@desc       Regiter a user
 //@access     Public
-//Faz a solicitação dos dados no servidor
-router.post(
-  "/",
-  [
-    check("name", "Digite um nome").not().isEmpty(), //.not.isEmpty é uma combinação que verifica se o campo está vazio
-    check("email", "Por favor, inclua um e-mail valido").isEmail(), // verifica se o item realizado foi digitado em formato de e-mail
-    check(
-      "password",
-      "Por favor, digite uma senha com pelo menos 6 caracteres"
-    ).isLength({ min: 6 }), //deixa em formato de senha, ou seja não conseguimos ver o que foi digitado e exige que seja digitado pelo menos 6 caracteres
-  ],
-  async (req, res) => {
-    //res.send("Usuário registrado");
-    //res.send(req.body);
-    //erros vai receber a validação do request
-    const errors = validationResult(req);
-    //se o errors foi diferente de vazio apresentará o erro 404(solicitação  incorreta)
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() }); //fornece uma matriz com os erros
-    }
-    //res.send("passed");
-    //desestruturando - os arquivos de dentro dos {} correspondem a req.body.nome ...
-    const { name, email, password } = req.body;
+router.post("/", (req, res) => {
+  res.send("Usuário registrado");
+});
 
-    try {
-      //verifica se há um usuário com esse e-mail. p findOne é um método do mongooose que permite encontrar um usuario atraves de qualquer campo
-      let user = await User.findOne({ email }); //essa função é uma promessa
-      if (user) {
-        // se houver um usuario com esse e-mail, retorna o erro 400 e a mensagem
-        return res.status(400).json({ msg: "Esse usuário já existe" });
-      }
-      /* Se o usuário não existir criaremos um novo usuário */
-      user = new User({
-        name, //poderiamos escrever name: name, email: email, password: password
-        email,
-        password,
-      });
-
-      /* Antes de salvar o novo usuário criado vamos criptografa-lo */
-
-      //isso definirá o quão segura é a senha (10 é um padrão)
-      const salt = await bcrypt.genSalt(10);
-      //nos dará a hash da senha onde temos senha e a criptografia criada na linha a cima genSalt
-      user.password = await bcrypt.hash(password, salt);
-      //salva isso no banco de dados
-      await user.save();
-      //res.send("Usuário salvo");
-
-      /* cria/envia o token do usuário */
-      const payload = {
-        //o token do usuário terá seu id
-        user: {
-          id: user.id,
-        },
-      };
-
-      /* passamos então o id que virará um token juntaente com a palavra secreta */
-      jwt.sign(
-        payload,
-        config.get(
-          "jwtSecret"
-        ) /* a palavra secreta permite descriptografar o token. Essa palavra secreta foi definida em default.json */,
-        {
-          /* O token  termina em 1 hora, ou seja, ficaremos logados por 1 hora */
-          expiresIn: 360000,
-        },
-        (err, token) => {
-          /* Caso ocorra algum erro na geração do  token */
-          if (err) throw err; /* Se houver um erro retorne o erro */
-          res.json({ token });
-        }
-      );
-    } catch (err) {
-      /* imprime o erro  */
-      console.error(err.message);
-      /*retorna erro no servidor */
-      res.status(500).send("Erro no servidor");
-    }
-  }
-);
-
-/* para aplicação funcionar deve-se exportar o router como na linha abaixo*/
 module.exports = router;
